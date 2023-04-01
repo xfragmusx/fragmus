661a428cdb05a31fe73bb370feaded0a2c025ce7
Fragmus <myEmail@example.com>
2023-04-01 17:05:25 +0200

    data_types_domashka

---
+++ domashka/data_types_domashka.py
@@ -0,0 +1,28 @@
+a = 10
+b = 20
+c = (a == b)
+print(c)
+
+KONSTANTA = "hello teatcher"
+print((KONSTANTA) + str(a - b))
+
+e = [10, 20, False, KONSTANTA]
+print(e)
+
+print({10, 20, False, KONSTANTA})
+
+d = (11)
+f = (22)
+g = (d + f)
+
+print([g])
+
+dictionary = {
+'Hello': 25,
+  'Good': 'Boy',
+    100: 'keys',
+     True: None,
+      False: [10],
+       float(2.5): {'Puplp': 'Fiction', 33: 22},
+        (55): (66)
+}
